31e243642267530ae510d9a3719165625d8c1c53
Suraj Bhandari <suraj.bhandari@worldlink.com.np>
2016-09-28 16:59:27 +0545

    Added animation on about page

--- app/src/main/java/com/example/user/myapps1st/About.java
+++ app/src/main/java/com/example/user/myapps1st/About.java
@@ -2,8 +2,15 @@ package com.example.user.myapps1st;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 public class About extends AppCompatActivity {
+    TextView title, version, desc;
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +18,13 @@ public class About extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("About");
+        title = (TextView) findViewById(R.id.title);
+        version = (TextView) findViewById(R.id.version);
+        desc = (TextView) findViewById(R.id.desc);
+        image = (ImageView) findViewById(R.id.image);
+
+        YoYo.with(Techniques.RollIn).duration(3000).playOn(image);
+        YoYo.with(Techniques.RollIn).duration(4000).playOn(title);
+        YoYo.with(Techniques.RollIn).duration(5000).playOn(version);
     }
 }
